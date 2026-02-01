0d675965025a3e9b553b09af38a54a10f47e843e
Vadim <shlehter@yandex.ru>
2026-02-01 16:48:20 +0300

    Fix ssues in deploy without dev servers

--- cli/frontend-setup.js
+++ cli/frontend-setup.js
@@ -14,7 +14,7 @@ NEXT_PUBLIC_BASE_URL=https://market.tlakepro.com
 NEXT_PUBLIC_DEFAULT_REGION=pl
 NEXT_PUBLIC_STRIPE_KEY=supersecret
 REVALIDATE_SECRET=supersecret
-NEXT_PUBLIC_SITE_NAME="TLakePro marketplace"
+NEXT_PUBLIC_SITE_NAME="TLakePro market"
 NEXT_PUBLIC_SITE_DESCRIPTION="TLakePro Marketplace"
 NEXT_PUBLIC_ALGOLIA_ID=
 NEXT_PUBLIC_ALGOLIA_SEARCH_KEY=`
@@ -47,8 +47,8 @@ export async function vendorPanelSetup(options) {
   await fs.writeFile(
     path.join(targetDir, "vendor-panel/.env"),
     `VITE_MEDUSA_BASE=/
-VITE_MEDUSA_STOREFRONT_URL=https://marketplace.tlakepro.com
-VITE_MEDUSA_BACKEND_URL=https://backend.marketplace.tlakepro.com
+VITE_MEDUSA_STOREFRONT_URL=https://market.tlakepro.com
+VITE_MEDUSA_BACKEND_URL=https://backend.market.tlakepro.com
 VITE_PUBLISHABLE_API_KEY=
 VITE_TALK_JS_APP_ID=
 VITE_DISABLE_SELLERS_REGISTRATION=false`
